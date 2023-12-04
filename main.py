8c8578a7e6c7665e6a951ac0025b2e3ebbff7709
Alvaro Aledo Tornero <118385592+Saturno10@users.noreply.github.com>
2023-12-04 17:45:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,149 +1,109 @@
-import regex as re
+from entonar import entonar, clasificar_entonacion
+from silabar import silabar
 import csv
-
-def entonacion(palabra,regla_silaba,regla_vocal):
-    #aplica la norma a cada sílaba para ver si hay una tilde (descarta esdrújulas y sobreesdrújulas)
-    for silaba in palabra:
-
-        tildes=regla_silaba.match(silaba.rstrip()).group("R1")
-        #si encuentra, la reemplaza por su mayúscula y devuelve la palabra
-        if(tildes!=""):
-            regla_silaba.sub(tildes.toupper(),tildes)
-            return palabra
-    #si no, intenta ver si la palabra es llana, si lo es, le aplica las normas de los diptongos y triptongos (descarta llanas)
-    llana=regla_silaba.match(palabra[::-2].rstrip()).group("R2")
-    if(llana!=""):
-        vocales=0;
-        for letra in palabra[::-2]:
-            if letra in ("a","e","i","o","u"):
-                vocales+=1
-        if (vocales==3):
-            segunda=False
-            for letra in silaba:
-                if (letra in ("a","e","i","o","u")):
-                    if segunda:
-                        palabra[::-2][letra] = letra.toupper()
-                        break
-                    else: segunda = True
-        elif (vocales==2):
-            tonica = regla_vocal.match(palabra[::-2].rstrop()).group("R4a")
-            if (tonica != ""):
-                regla_vocal.sub(tonica.toupper(),tonica)
-                return palabra
-            else:
-                tonica = regla_vocal.match(palabra[::-2].rstrop()).group("R4b")
-                regla_vocal.sub(tonica.toupper(), tonica)
-                return palabra
-        else:
-            for letra in palabra[::-2]:
-                if letra in ("a","e","i","o","u"):
-                    palabra[::-2][letra] = letra.toupper()
-                    return palabra
-    else: #si no se cumple ninguna condición anterior, la palabra tiene que ser aguda, y se hace de vuelta lo anterior pero en la última sílaba
-        vocales = 0;
-        for letra in palabra[::-1]:
-            if letra in ("a", "e", "i", "o", "u"):
-                vocales += 1
-        if (vocales == 3):
-            segunda = False
-            for letra in silaba:
-                if (letra in ("a", "e", "i", "o", "u")):
-                    if segunda:
-                        palabra[::-2][letra] = letra.toupper()
-                        break
-                    else:
-                        segunda = True
-        elif (vocales == 2):
-            tonica = regla_vocal.match(palabra[::-1].rstrop()).group("R4a")
-            if (tonica != ""):
-                regla_vocal.sub(tonica.toupper(), tonica)
-                return palabra
-            else:
-                tonica = regla_vocal.match(palabra[::-1].rstrop()).group("R4b")
-                regla_vocal.sub(tonica.toupper(), tonica)
-                return palabra
-        else:
-            for letra in palabra[::-2]:
-                if letra in ("a", "e", "i", "o", "u"):
-                    palabra[::-2][letra] = letra.toupper()
-                    return palabra
-
-
-
-
-
-
-
-
-def silabar(palabra,regla_silabacion):
-    pass
-
-def procesar(reglas, entrada):
-    #quitar palabras repetidas
-    conjuntoEntrada = set(entrada.split('\n'))
-
-    #todo escribir en un archivo de texto
-
-    #escribir en un archivo de salida si ya existe
-    archivo = open('salida.cvs','a+')
-    escritor = csv.writer(archivo)
-    lector = csv.reader(archivo)
-    #si se acaba de crear, añadirle la cabecera
-    if (len(list(lector)) == 0):
-        escritor.writerow(['Palabra', 'Silabación', 'Entonación']);
-
-    palabras = []
-    #lee desde la segunda linea (la primera es la cabecera)
-    try:
-        for palabra in next(lector):
-            palabras.append(palabra[0])
-    except StopIteration:
-        pass
-
-
-
-    for palabra_entrada in conjuntoEntrada:
-        if palabra_entrada not in palabras:# ver si la palabra ya está
-            palabra_silabada= silabar(palabra_entrada,reglas[0])
-            palabra_entonada= entonacion(palabra_silabada,reglas[1],reglas[2])
-            escritor.writerow([palabra_entrada,palabra_silabada , palabra_entonada])
-
-
-
-
-
-
-
-
-
-
-
-    #leer fichero, separar en su totalidad en una lista y quedarnos con las palabras normales
-
-
-
-
-
-
+import ast
 
 if __name__ == '__main__':
-    patron_silabas = r''
-    reglas_silabas = re.compile(patron_silabas)
-    patron_entonacion_silabas=r'(?P<R1> [áéíóú])|(?P<R2> *[snaeiou]$)|(?P<R3> *[qwrtypdfghjklñzxcvbm]$)'
-    reglas_entonacion_silabas = re.compile(patron_entonacion_silabas)
-    patron_vocal_tonica= r'(?P<R4>(?P<R4a> [aeo])|(?P<R4b> [iu][iu]))'
-    reglas_vocal_tonica = re.compile(patron_vocal_tonica)
-    reglas =[reglas_silabas,reglas_entonacion_silabas,reglas_vocal_tonica]
+    diccionario = {}
+    # Intentamos leer el archivo que contiene el diccionario de anteriores ejecuciones
+    try:
+        with open('diccionario.csv', 'r') as archivo:
+            # Si lo encontramos, lo leemos
+            lector = csv.reader(archivo)
 
-    try: #leer archivo de texto con las palabras de entrada
-        entrada = open('vocabulario.txt','r')
-    except FileNotFoundError: #si no encuentra archivo, leer desde teclado las palabras
-        entrada = ""
-        ultima_entrada = ""
-        while (True):
-            ultima_entrada = str(input("Introduce las palabras que quieres procesar una a una separadas por un enter. Si quieres terminar pon 0: "))
-            if ultima_entrada == "<exit>":
-                break
-            entrada +=ultima_entrada + "\n"
+            # Saltar la cabecera
+            next(lector)
 
-    procesar(reglas,entrada)
+            # Leer cada fila del archivo
+            for fila in lector:
+                # por si había algún fallo en la escritura
+                if len(fila) < 3:
+                    break
+                # Y añadimos al diccionario de esta ejecución todo lo que encontramos
+
+                palabra = fila[0]
+                silabas = ast.literal_eval(fila[1])  # Convertir la string a una lista
+                entonacion = ast.literal_eval(fila[2])  # Convertir la string a una lista
+
+                # Añadir la palabra al diccionario
+                diccionario[palabra] = {"silabas": silabas, "entonación": entonacion}
+            print("Diccionario encontrado, leyendo datos... \n")
+        archivo.close()
+    # Si no lo encontramos, creamos uno nuevo y le añadimos la cabecera
+    except FileNotFoundError:
+        with open('diccionario.csv', 'w', newline='') as archivo:
+            escritor = csv.writer(archivo)
+            escritor.writerow(["Palabra", "Sílabas", "Entonación"])
+            print("Diccionario no encontrado, construyendo diccionario vacío... \n")
+        archivo.close()
+    # Preparamos el archivo para escribirle lineas en caso de que añadan palabras nuevas
+    with open('diccionario.csv', 'a', newline='') as archivo:
+
+        escritor = csv.writer(archivo)
+        # Seleccionamos modo
+        entrada = None
+        while entrada not in ("1", "2", "3", "4", "5"):
+            print("---EJECUTANDO PROGRAMA DE PRÁCTICA DE AUTÓMATAS Y LENGUAJES FORMALES---\n1. Silabear una palabra.\n"
+                  "2. Clasificar palabra según su entonación\n3. Obtener palabras que rimen con una palabra\n4. "
+                  "Justificar texto\n5. Salir")
+
+            entrada = str(input("Elige la opción que quieres escribiendo su número: "))
+            match entrada:
+                # Silabar
+                case "1":
+                    palabra = ""
+                    while palabra != "0":
+                        palabra = str(input("Introduce la palabra que quieres silabear, procura que esté bien escrita."
+                                            " Pon un 0 para terminar: "))
+                        if palabra != "0":
+                            # Si no está en el diccionario, lo insertamos. Sino simplemente leemos el diccionario
+                            if palabra not in diccionario:
+                                print("Palabra no encontrada en diccionario, procesando e insertando...")
+                                silabas = silabar(palabra)
+                                entonadas = entonar(silabas)
+                                diccionario[palabra] = {"silabas": silabas, "entonacion": entonadas}
+                                escritor.writerow([palabra, silabas, entonadas])
+                                print("La palabra ", palabra, " silabada es: ", silabas)
+                            else:
+                                print("La palabra ya está en el diccionario, leyendo resultado...")
+                                print("La palabra ", palabra, " silabada es: ", diccionario[palabra]["silabas"])
+                    break
+                # Entonar
+                case "2":
+                    palabra = ""
+                    while palabra != "0":
+                        palabra = str(input(
+                            "Introduce la palabra que quieres clasificar según su entonación,"
+                            " procura que esté bien escrita. Pon un 0 para terminar: "))
+                        if palabra != "0":
+                            # Si no está en el diccionario, lo insertarmos. Sino simpplemente leemos el diccionario
+                            if palabra not in diccionario:
+                                print("Palabra no encontrada en diccionario, procesando e insertando...")
+                                silabas = silabar(palabra)
+                                entonadas = entonar(silabas)
+                                diccionario[palabra] = {"silabas": silabas, "entonacion": entonadas}
+                                escritor.writerow([palabra, silabas, entonadas])
+                                print("La palabra ", palabra, " entonada es: ", entonadas, "por lo que es:",
+                                      clasificar_entonacion(entonadas), "\n")
+                            else:
+                                print("La palabra ya está en el diccionario, leyendo resultado...")
+                                print("La palabra ", palabra, " entonada es: ", diccionario[palabra]["entonacion"],
+                                      "por lo que es:", clasificar_entonacion(diccionario[palabra]["entonacion"]), "\n")
+                    break
+                # Rimas
+                case "3":
+                    print("No implementado")
+                    break
+                # Justificar
+                case "4":
+                    print("No implementado")
+                    break
+                # Salir
+                case "5":
+                    print("Iniciando fin de programa")
+                    break
+                case _:
+                    print("Por favor, inserta un valor entre 1 y 3")
+            archivo.write('\n')
+    archivo.close()
+    print("Saliendo del programa...")
